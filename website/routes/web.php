66e8d64f39485d10503b5019d97303600e6b73a2
Pierre Queruel <pierre.queruel@commoprices.com>
2022-07-13 20:33:33 +0200

    adding initial route but not design us

--- website/routes/web.php
+++ website/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,53 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'middleware' => ['auth'],
+    'as' => 'auth.',
+    'prefix' => 'web',
+], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+    Route::get('/logout', function () {
+        Auth::logout();
+        return redirect(route('guest.home'));
+    })->name('logout');
+});
+
+
+Route::group([
+    'middleware' => ['guest'],
+    'as' => 'guest.',
+], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+    Route::get('/register', function () {
+        return view('welcome');
+    })->name('register');
+    Route::post('/register', function () {
+        return view('welcome');
+    });
+    Route::get('/login', function () {
+        return view('welcome');
+    })->name('login');
+    Route::post('/login', function (Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect(route('auth.home'));
+        }
+        return view('welcome');
+    })->name('login.post');
+    Route::get('/forget', function () {
+        return view('welcome');
+    });
+    Route::post('/forget', function () {
+        return view('welcome');
+    });
 });
